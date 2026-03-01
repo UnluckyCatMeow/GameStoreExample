db8b4a85b55ad9dd04d145af463b697bb7619726
Nik <nik348791@gmail.com>
2026-03-01 20:01:10 +0200

    Add user wishlist

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -11,6 +11,17 @@
             Game game = new Game("Minecraft", 999, "Ахуєть, работає");
             game.PrintInfo();
             Console.WriteLine("-------------");
+
+            User dasha = new User(20, "Даша");
+            Game portal = new Game("Portal", 125, "Гра головоломка");
+            Game stray = new Game("Stray", 250, "Гра про кота");
+
+            dasha.AddGameToWishlist(portal);
+            dasha.AddGameToWishlist(stray);
+            dasha.PrintWishlist();
+            dasha.DeleteGameFromWishlist(portal);
+            dasha.PrintWishlist();
+
         }
     }
 }
--- GameStore/User.cs
+++ GameStore/User.cs
@@ -2,13 +2,34 @@
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public List<Game> Wishlist { get; private set; }
+        
         public User(int id, string name)
         {
             Id = id;
             Name = name;
+            Wishlist = new List<Game>();
+        }
+
+        public void AddGameToWishlist(Game game)
+        {
+            Wishlist.Add(game);
+
+        }
+        public void DeleteGameFromWishlist(Game game)
+        {
+            Wishlist.Remove(game);
+        }
+        public void PrintWishlist()
+        {
+            Console.WriteLine("Обранно: ");
+            foreach(var game in Wishlist)
+            {
+                Console.WriteLine($"- {game.Name}, {game.Description}");
+            }
+           
         }
     }
 }
